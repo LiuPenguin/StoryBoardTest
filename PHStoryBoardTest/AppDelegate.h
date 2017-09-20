4583a873277d003c9b9e1040de4e2f7ea9463e1b
钱趣多 <qianquduo@qianquduodeMacBook-Pro-2.local>
2017-09-20 09:50:45 +0800

    Initial Commit

---
+++ PHStoryBoardTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PHStoryBoardTest
+//
+//  Created by 钱趣多 on 2017/9/20.
+//  Copyright © 2017年 LPH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
